ee4f5ca9fe43b5e64fb5e477f601281d107dc7e4
moralss <jeramoral@gmail.com>
2016-02-12 09:45:48 -0500

    Changed the global variables

--- Inyoka noga.py
+++ Inyoka noga.py
@@ -9,9 +9,9 @@ import random, pygame, sys
 from pygame.locals import *
 
 FramesPerSecond = 15
-WINDOWWIDTH = 1200
-WINDOWHEIGHT = 800
-CELLSIZE = 20
+WindowWidth = 1200
+WindowHeight = 800
+CellSize = 20
 assert WINDOWWIDTH % CELLSIZE == 0, "Window width must be a multiple of cell size."
 assert WINDOWHEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size."
 CELLWIDTH = int(WINDOWWIDTH / CELLSIZE)
